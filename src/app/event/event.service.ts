f6a9e21363c53fb76fe6a14aa3d149166ad91650
s2it_ppoli <s2it_ppoli@uolinc.com>
2017-10-25 18:10:07 -0200

    adicionado pagina atual

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -9,12 +9,14 @@ import { AuthenticationService } from '../auth/auth.service';
 @Injectable()
 export class EventService {
 
+    actualPage = 0;
+
     constructor(private http: Http,private authenticationService: AuthenticationService) {
     }
  
     getEvents() {
         let headers = new Headers({ 'Authorization': this.authenticationService.token });
         let options = new RequestOptions({ headers: headers });
-        return this.http.get(apibaseurl+'/event/user?page=0&size=20', options).map((response: Response) => response.json());
+        return this.http.get(apibaseurl+'/event/user?page='+this.actualPage+'&size=20', options).map((response: Response) => response.json());
     }
 }
